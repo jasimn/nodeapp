cd7a5d81b0d3eee0c56386cbd5dffa1b7647b1d4
jasimn <113772434+jasimn@users.noreply.github.com>
2026-01-11 19:30:28 +0530

    Update app.js

--- mynodeapp/app.js
+++ mynodeapp/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from private eks + Jenkins");
+  res.send("Hello from private eks ");
 });
 
 app.listen(3000, () => console.log("App running on 3000"));
