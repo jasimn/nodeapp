2a2ffdac6b26a4d668e6a73d465942031102987f
jasimn <113772434+jasimn@users.noreply.github.com>
2026-01-30 18:28:51 +0530

    Delete mynodeapp directory

--- mynodeapp/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello from private eks ");
-});
-
-app.listen(3000, () => console.log("App running on 3000"));
